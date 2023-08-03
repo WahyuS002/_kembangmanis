4d9bb946f9982231f375068ec5ec5e2ce0031994
WahyuS002 <syahputrawahyu61@gmail.com>
2023-08-04 00:34:17 +0700

    feat: 90% hamburger menu

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Icons } from "./icons";
+import { useState } from "react";
 
 export default function Nav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <>
       <nav className="py-10 items-center justify-between font-medium container mx-auto hidden md:flex">
@@ -25,10 +28,20 @@ export default function Nav() {
           </Button>
         </ul>
       </nav>
-      <nav className="px-8 pt-10 flex items-center justify-between">
+      <nav className="px-8 pt-10 flex items-center justify-between lg:hidden">
         <div className="bg-zinc-900 w-6 h-6 rounded-full" />
-        <Icons.menu className="cursor-pointer" />
+        <Icons.menu
+          className="cursor-pointer"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        />
       </nav>
+      {isMenuOpen && (
+        <ul className="px-8 py-4">
+          <li>Lorem, ipsum dolor.</li>
+          <li>Lorem, ipsum dolor.</li>
+          <li>Lorem, ipsum dolor.</li>
+        </ul>
+      )}
     </>
   );
 }
