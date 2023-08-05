1745f9f30ccee02bc9a6255445476a38ba33eb2f
WahyuS002 <syahputrawahyu61@gmail.com>
2023-08-05 19:27:25 +0700

    feat: using image for struktur pemerintah desa

--- app/(front-page)/structures/page.tsx
+++ app/(front-page)/structures/page.tsx
@@ -1,6 +1,7 @@
 import ProfileCard from "@/components/profile-card";
 import { TypographyH1 } from "@/components/ui/typography";
-import profiles from "@/data.json";
+import Image from "next/image";
+import strukturPemerintahImg from "@/public/images/struktur-pemerintah.png";
 
 export default function StructuresPage() {
   return (
@@ -8,8 +9,10 @@ export default function StructuresPage() {
       <TypographyH1 className="text-center max-w-3xl mx-auto pb-20">
         Struktur Pemerintah Desa Kembang Manis
       </TypographyH1>
-      {profiles &&
-        profiles.map((profile, idX) => <ProfileCard key={idX} {...profile} />)}
+      <Image
+        src={strukturPemerintahImg}
+        alt="Struktur Pemerintah Desa Kembang Manis"
+      />
     </div>
   );
 }
